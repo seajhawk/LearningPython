fc1c7505a4699d1b30a4622d3ea7324b8d52ac98
seajhawk <chris@harriskids.com>
2019-03-25 14:50:31 -0700

    Carters updated version

--- Queue with Nodes.py
+++ Queue with Nodes.py
@@ -4,11 +4,13 @@ Created on Sun Mar 24 10:34:15 2019
 
 @author: carte
 """
+
+
+
 class Node:
     def __init__(self, value):
         self.data = value
         self.front = None
-
 
 class Queue:
     def __init__(self):
@@ -28,14 +30,27 @@ class Queue:
         self.tail = new_node
         self.count += 1
 
+
     def de_queue(self):
         if not self.is_empty():
-            self.head = self.head.front
-            self.count -= 1
-        else:
-            print("Empty")
 
-    def is_Empty(self):
+            data = self.head.data
+
+            if self.count > 1:
+                self.head = self.head.front
+
+            else:
+                self.head = None
+                self.tail = None
+
+            self.count -= 1
+            return data
+
+        else:
+            return None
+
+
+    def is_empty(self):
         if self.head is None and self.tail is None:
             return True
         else:
@@ -43,5 +58,14 @@ class Queue:
 
     def size(self):
         return self.count
-    
 
+
+q = Queue()
+q.en_queue("a")
+q.en_queue("b")
+q.en_queue("c")
+print(q.count)
+print(q.de_queue())
+print(q.de_queue())
+print(q.de_queue())
+q.is_empty()
